25f344ac0c909461a6d76078d21dd83378063a76
Dani Bilibok <bilibok.dani@gmail.com>
2020-07-15 13:57:22 +0200

    Hello others

---
+++ week01/day04/src/helloOthers.java
@@ -0,0 +1,10 @@
+public class helloOthers {
+    public static void main(String[] args) {
+        String name1 = "Esther";
+        String name2 = "Mary";
+        String name3 = "Joe";
+        System.out.println("Hello, " + name1 + "!");
+        System.out.println("Hello, " + name2 + "!");
+        System.out.println("Hello " + name3 + "!");
+    }
+}
