0f75beb0f3ada2b6e8d404d0a696ebc48fbe05de
Dani Bilibok <bilibok.dani@gmail.com>
2020-07-15 15:11:47 +0200

    two numbers

---
+++ week01/day04/src/twoNumbers.java
@@ -0,0 +1,14 @@
+public class twoNumbers {
+    public static void main(String[] args) {
+        int num1 = 13;
+        int num2 = 22;
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num2 * num1);
+        System.out.println((double)num2 / num1);
+        System.out.println(num2 / num1);
+        System.out.println(num2 % num1);
+
+    }
+}
