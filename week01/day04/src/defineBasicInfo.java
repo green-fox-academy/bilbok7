df4324c44b0d037a377cede2e63ff3ca53a23d5a
Dani Bilibok <bilibok.dani@gmail.com>
2020-07-15 16:35:58 +0200

    define basic info

---
+++ week01/day04/src/defineBasicInfo.java
@@ -0,0 +1,13 @@
+public class defineBasicInfo {
+
+    public static void main(String[] args) {
+        String name = "Bilibok Dániel";
+        int age = 22;
+        double height = 1.9;
+        boolean married = false;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(married);
+    }
+}
