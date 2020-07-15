e9700b0e6be62fc1ac028876e266688925266514
Dani Bilibok <bilibok.dani@gmail.com>
2020-07-15 14:03:53 +0200

    introduce

---
+++ week01/day04/src/introduceYourself.java
@@ -0,0 +1,10 @@
+public class introduceYourself {
+    public static void main(String[] args) {
+        String name = "Bilibok Dániel";
+        int age = 22;
+        double height = 1.89;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+}
