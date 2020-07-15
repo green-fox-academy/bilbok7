2098d1cf0e682ffdacf40b63a332ee330af4c87a
Dani Bilibok <bilibok.dani@gmail.com>
2020-07-15 15:47:37 +0200

    bmi

---
+++ week01/day04/src/bmi.java
@@ -0,0 +1,8 @@
+public class bmi {
+    public static void main(String[] args) {
+        double massInKg = 81.2;
+        double heightInM = 1.78;
+
+        System.out.println(81.2 / (1.78*1.78));
+    }
+}
