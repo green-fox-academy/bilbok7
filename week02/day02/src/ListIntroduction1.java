5a1a6d99498c05e5a757a22276600374a146b111
Dani Bilibok <bilibok.dani@gmail.com>
2020-07-21 17:31:50 +0200

    done for today

--- week02/day02/src/ListIntroduction1.java
+++ week02/day02/src/ListIntroduction1.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class ListIntroduction1 {
+  public static void main(String[] args) {
+    ArrayList<String> names = new ArrayList<>();
+    System.out.println("Size: " + names.size());
+    names.add("William");
+    if (names.isEmpty()) {
+      System.out.println("It's empty");
+    } else {
+      System.out.println("It's not empty");
+    }
+    names.add("John");
+    names.add("Amanda");
+    System.out.println("Size now: " + names.size());
+    System.out.println("Third is: " + names.get(2));
+    for (String name : names){
+      System.out.println(name);
+    }
+    for (int i = 0; i < names.size(); i++) {
+      System.out.println((i + 1) + ". " + names.get(i));
+    }
+    names.remove(1);
+
+    for (int i = names.size() - 1; i >= 0; i--) {
+      System.out.println(names.get(i));
+    }
+    names.clear();
+  }
 }
--- week02/day02/src/MapIntroduction1.java
+++ week02/day02/src/MapIntroduction1.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapIntroduction1 {
+  public static void main(String[] args) {
+
+    Map<Integer, Character> myMap = new HashMap<>();
+
+    if (myMap.isEmpty()) {
+      System.out.println("The map is empty.");
+    } else {
+      System.out.println("The map is not empty.");
+    }
+
+    myMap.put(97, 'a');
+    myMap.put(98, 'b');
+    myMap.put(99, 'c');
+    myMap.put(65, 'A');
+    myMap.put(66, 'B');
+    myMap.put(67, 'C');
+
+    for (Integer key : myMap.keySet()) {
+      System.out.print(key + " ");
+    }
+    System.out.println();
+
+    for (Integer key : myMap.keySet()) {
+      System.out.print(myMap.get(key) + " ");
+    }
+    System.out.println();
+
+    myMap.put(68, 'D');
+
+    System.out.println("Number of keys: " + myMap.keySet().size());
+
+    System.out.println(myMap.get(99));
+
+    myMap.remove(97);
+
+    if (myMap.containsKey(100)) System.out.println("There is a value with key 100.");
+    else System.out.println("There is no value with key 100.");
+
+    myMap.clear();
+
+  }
 }
