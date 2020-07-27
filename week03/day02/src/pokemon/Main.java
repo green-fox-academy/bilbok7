fa1b4bb719447edc8954fc12097152f123817e56
Dani Bilibok <bilibok.dani@gmail.com>
2020-07-27 18:09:11 +0200

    pokemon exercise done

--- week03/day02/src/pokemon/Main.java
+++ week03/day02/src/pokemon/Main.java
@@ -1,5 +1,8 @@
 package pokemon;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
   public static void main(String[] args) {
     List<Pokemon> pokemonOfAsh = initializePokemons();
@@ -13,19 +16,29 @@ public class Main {
     Pokemon wildPokemon = new Pokemon("Oddish", "leaf", "water");
 
     // Which pokemon should Ash use?
+    Pokemon winingPokemon = null;
+    for (Pokemon pokemon : pokemonOfAsh) {
+      if (pokemon.isEffectiveAgainst(wildPokemon)) {
+        winingPokemon = pokemon;
+      }
+    }
 
-    System.out.print("I choose you, ");
+    if (winingPokemon != null) {
+      System.out.print("I choose you, " + winingPokemon.name);
+    } else {
+      System.out.println("F##k!");
+    }
   }
 
-  private static List<Pokemon> initializePokemons() {
-    List<Pokemon> pokemon = new ArrayList<>();
+    private static List<Pokemon> initializePokemons() {
+      List<Pokemon> pokemon = new ArrayList<>();
 
-    pokemon.add(new Pokemon("Balbasaur", "leaf", "water"));
-    pokemon.add(new Pokemon("Pikatchu", "electric", "water"));
-    pokemon.add(new Pokemon("Charizard", "fire", "leaf"));
-    pokemon.add(new Pokemon("Balbasaur", "water", "fire"));
-    pokemon.add(new Pokemon("Kingler", "water", "fire"));
+      pokemon.add(new Pokemon("Balbasaur", "leaf", "water"));
+      pokemon.add(new Pokemon("Pikatchu", "electric", "water"));
+      pokemon.add(new Pokemon("Charizard", "fire", "leaf"));
+      pokemon.add(new Pokemon("Balbasaur", "water", "fire"));
+      pokemon.add(new Pokemon("Kingler", "water", "fire"));
 
-    return pokemon;
-  }
+      return pokemon;
+    }
 }
